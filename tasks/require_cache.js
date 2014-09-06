58ab58503729753efa095c9b15e1d7bb6f4bc598
Zach Wentz <zkwentz@gmail.com>
2014-09-06 20:27:41 +0000

    updated to read require and hash those and update

--- tasks/require_cache.js
+++ tasks/require_cache.js
@@ -8,22 +8,141 @@
 
 'use strict';
 
-var requireCache = require('/var/require-cache');
+var requirejs   = require('requirejs/bin/r.js');
+var fs          = require('fs');
+var chalk       = require('chalk');
+var path        = require('path');
+var file        = require('file-utils');
+var getHash     = require('../lib/hash');
+
+function unixify(path) {
+  return path.split('\\').join('/');
+}
 
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
+    
+  
 
-  grunt.registerMultiTask('require_cache', 'Change requirejs paths to cache-busted paths.', function() {
+  grunt.registerMultiTask('require_cache', 'Prepend a unique hash to a file, and update requirejs paths to cache-busted paths.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-        config: this.data.rjsConfig,
-        assets: this.data.assetsMap,
-        baseUrl: ''
+        srcBasePath: "",
+        destBasePath: "",
+        flatten: false,
+        hashLength: 8
+    }, this.async());
+    
+    
+    function hashFile(src) {
+        var source = grunt.file.read(src);
+        var hash = getHash(source, 'utf8').substr(0, options.hashLength);
+        var dirname = path.dirname(src);
+        var rootDir = path.relative(options.srcBasePath, dirname);
+        var ext = path.extname(src);
+        var basename = path.basename(src, ext);
+        
+        // Default destination to the same directory
+        var dest = file.dest || path.dirname(src);
+        
+        var newFile = hash + '.' + basename + ext;
+        var outputPath = path.join(dest, newFile);
+        
+        // Determine if the key should be flatten or not. Also normalize the output path
+        var key = path.join(rootDir, path.basename(src));
+        var outKey = path.relative(options.destBasePath, outputPath);
+        if (options.flatten) {
+          key = path.basename(src);
+          outKey = path.basename(outKey);
+        }
+        
+        grunt.file.copy(src, outputPath);
+        grunt.log.writeln('Generated: ' + outputPath);
+        
+        map[unixify(key)] = unixify(outKey);
+        
+        return outputPath;
+    }
+    
+    var map = {};
+    var mappingExt = path.extname(options.mapping);
+    var config;
+
+    // If mapping file is a .json, read it and just override current modifications
+    if (mappingExt === '.json' && grunt.file.exists(options.mapping)) {
+      map = grunt.file.readJSON(options.mapping);
+    }
+    
+    // Get hashed requirejs path
+    if (options.rjsConfigPath) {
+        
+        // Grab the config file
+        if (file.exists(options.rjsConfigPath)) {
+        
+            // First hash the config file itself
+            var hashedConfigPath = hashFile(options.rjsConfigPath,map);
+            config = grunt.file.read(hashedConfigPath);
+            
+            var rjsConfig;    
+            requirejs.tools.useLib(function(require){
+        
+                rjsConfig = require('transform').modifyConfig(config,function(config){  
+                   var relativeBaseUrl = path.join('.',config.baseUrl);         
+                   for (var aPath in config.paths) {
+                     var pathToJSFile = relativeBaseUrl + '/' + aPath + '.js';
+                     if (file.exists(pathToJSFile) && grunt.file.isFile(pathToJSFile)) {
+                         var relativeHashedPath = path.relative(relativeBaseUrl,hashFile(pathToJSFile,map));
+                         config.paths[aPath] = relativeHashedPath;
+                     } else {
+                         console.log(chalk.yellow(pathToJSFile + " not found, skipping."));
+                     }
+                   }           
+                   
+                   return config;
+                });
+                
+                grunt.file.write(hashedConfigPath,rjsConfig);
+                console.log(chalk.green('Updated ' + hashedConfigPath + ' with cache bust paths'));
+            });
+            
+            
+        } else {
+            //log error no config file found at specified path
+            console.log(chalk.yellow('No config file found at: '+options.rjsConfigPath));
+        }
+        
+    }
+    else 
+    {
+        //log error no config file specified
+        console.log(chalk.red('No config file path specified.'));
+    }
+    
+    
+    this.files.forEach(function(file) {
+      file.src.forEach(function(src) {
+          hashFile(src, map);
+      });
     });
 
-    requireCache(options,this.async());
+    if (options.mapping) {
+      var output = '';
+
+      if (mappingExt === '.php') {
+        output = "<?php return json_decode('" + JSON.stringify(map) + "'); ?>";
+      } else {
+        output = JSON.stringify(map, null, "  ");
+      }
+
+      grunt.file.write(options.mapping, output);
+      grunt.log.writeln('Generated mapping: ' + options.mapping);
+    }
+    
+
   });
+  
+  
 
 };
